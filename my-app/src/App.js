fc72aadd2fbcada8da952c9fb7798b6051cbef31
Kira <kirakari1101@gmail.com>
2021-08-20 23:19:36 +0300

    add burger menu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Example from './Burger'
 
 function App() {
   return (
     <div className="App">
-      fjkdskafj
+      {/* <Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } />
+      <main id="page-wrap">
+        sdfjdasfkj
+      </main> */}
+      <Example pageWrapId={"page-wrap"} outerContainerId={ "App" }/>
+      <div id="page-wrap">Hello</div>
     </div>
   );
 }
